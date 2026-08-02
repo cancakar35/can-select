1e34113d177a1d372e34fe30957b2ef2b13647ce
Can Çakar <54742785+cancakar35@users.noreply.github.com>
2026-08-02 15:45:15 +0300

    Add licence banner

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,16 @@ export default defineConfig({
       formats: ['es', 'umd'],
       fileName: (format) => `can-select.${format}.js`,
     },
+    rolldownOptions: {
+      output: {
+        postBanner: `/**
+* can-select ${process.env.npm_package_version}
+* Released under the MIT License.
+*
+* https://github.com/cancakar35/can-select
+*/`,
+      },
+    },
   },
   plugins: [dts({ bundleTypes: true })],
 });
